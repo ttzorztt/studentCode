2ffaa176fcaa5d1f6c036cea6657e22071aa1a7d
ttzorztt <95260255+ttzorztt@users.noreply.github.com>
2021-11-30 09:37:16 +0800

    Add files via upload

---
+++ java/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "脚本需要管理员密码来获取可执行权限,若已获得请忽略!"
+sudo chmod a+x run.sh
+cd src
+javac Common.java
+javac EnemyFish.java
+javac MyFish.java
+javac StartGame.java
+java StartGame
+cd ..
